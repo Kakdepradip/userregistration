786d26ba6229b5e63462b666fa9bfa54a8026a13
Praddip kakde <kakdepradip21@gmail.com>
2026-02-27 12:17:08 +0530

    header works done

--- userRegistration/src/app/app.ts
+++ userRegistration/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatIconModule,MatButtonModule,MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
